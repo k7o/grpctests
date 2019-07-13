c79046a3511b26b9d6fccd2c7547fc2ce3d0768f
Eric Kerst <eric.kerst@gmail.com>
2019-07-13 17:56:48 +0200

    tussenstop

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,14 +1,42 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Messages;
 using System;
+using System.IO;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        const int Port = 50051;
+
+        static async Task Main(string[] args)
         {
-          
+            AppContext.SetSwitch(
+              "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
+              true);
+            
+            var cacert = File.ReadAllText($@"Certificates\ca.crt");
+            var cert = File.ReadAllText($@"Certificates\client.crt");
+            var key = File.ReadAllText($@"Certificates\client.key");
+
+            var keypair = new KeyCertificatePair(cert, key);
+            var sslCredentials = new SslCredentials(cacert, keypair);
+
+            var c = new Channel("meh", Port, sslCredentials);
+
+            var httpClient = new HttpClient();
+
+            // The port number(50051) must match the port of the gRPC server.
+            httpClient.BaseAddress = new Uri($"http://localhost:{Port}");
+            var client = GrpcClient.Create<Fucker.FuckerClient>(httpClient);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
--- Server/Services/GreeterService.cs
+++ Server/Services/GreeterService.cs
@@ -1,18 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
+using Messages;
 
 namespace Server
 {
-    public class GreeterService : Greeter.GreeterBase
+    public class GreeterService : Fucker.FuckerBase
     {
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
             {
-                Message = "Hello " + request.Name
+                Message = "Hello " + request.Name + ", age " + request.Age
             });
         }
     }
